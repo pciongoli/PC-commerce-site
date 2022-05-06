79c60bae02b1b866d1bba9759488153e2aa1509b
pcion <patrick.ciongoli@gmail.com>
2022-05-06 01:56:19 -0400

    completed category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,16 @@ router.get("/", (req, res) => {
    // find all categories
    // be sure to include its associated Product
 
-   Category.findAll({ include: [{ model: Product }] })
+   Category.findAll({
+      attributes: ["id", "category_name"],
+      include: [
+         {
+            model: Product,
+            attributes: ["id", "product_name", "price", "stock", "category_id"],
+         },
+      ],
+   })
+
       .then((dbCategoryData) => res.json(dbCategoryData))
       .catch((err) => {
          console.log(err);
@@ -27,34 +36,45 @@ router.get("/:id", (req, res) => {
       },
       include: {
          model: Product,
+         attributes: ["id", "product_name", "price", "stock", "category_id"],
       },
-   }).then((dbCategoryOne) => {
-      res.json(dbCategoryOne);
-   });
+   })
+      .then((dbCategoryData) => {
+         if (!dbCategoryData) {
+            res.status(404).json({ message: "This id does not exist!" });
+            return;
+         }
+         res.json(dbCategoryData);
+      })
+      .catch((err) => {
+         console.log(err);
+         res.status(500).json(err);
+      });
 });
 
 router.post("/", (req, res) => {
    // create a new category
    Category.create({
       category_name: req.body.category_name,
-   }).then((dbCategoryData) => res.json(dbCategoryData));
+   })
+      .then((dbCategoryData) => res.json(dbCategoryData))
+      .catch((err) => {
+         console.log(err);
+         res.status(500).json(err);
+      });
 });
 
 router.put("/:id", (req, res) => {
    // update a category by its `id` value
-   Category.update(
-      {
-         category_name: req.body.category_name,
-      },
-      {
-         // get category based on
-         where: {
-            id: req.params.id,
-         },
-      }
-   )
-      .then((updatedCategory) => {
-         res.json(updatedCategory);
+   Category.update({
+      category_name: req.body.category_name,
+   })
+      .then((dbCategoryData) => {
+         if (!dbCategoryData) {
+            res.status(404).json({ message: "This id does not exist!" });
+            return;
+         }
+         res.json(dbCategoryData);
       })
       .catch((err) => {
          console.log(err);
